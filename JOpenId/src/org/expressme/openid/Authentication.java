b40d1a228d1b76b22ad845176e757ac271a1f3ef
erwin.quinto <erwin.quinto@223a7d90-fd68-11dd-8259-6defae454439>
2009-12-09 11:59:43 +0000

    Add support for fullname, firstname, lastname, gender, and language attribute.

--- JOpenId/src/org/expressme/openid/Authentication.java
+++ JOpenId/src/org/expressme/openid/Authentication.java
@@ -1,15 +1,24 @@
 package org.expressme.openid;
 
+import java.io.Serializable;
+
 /**
  * Authentication information returned from OpenID Provider.
  * 
  * @author Michael Liao (askxuefeng@gmail.com)
+ * @author Erwin Quinto (erwin.quinto@gmail.com)
  */
-public class Authentication {
+public class Authentication implements Serializable {
 
-    private String identity;
+	private static final long serialVersionUID = -7031455449710566518L;
+	private String identity;
     private String email;
-
+    private String fullname;
+    private String firstname;
+    private String lastname;
+    private String language;
+    private String gender;
+    
     /**
      * Get identity.
      */
@@ -30,12 +39,85 @@ public class Authentication {
      */
     public void setEmail(String email) { this.email = email; }
 
+    /**
+     * Get fullname.
+     */
+    public String getFullname() {
+		return fullname;
+	}
+
+    /**
+     * Set fullname.
+     */
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+    /**
+     * Get firstname.
+     */
+	public String getFirstname() {
+		return firstname;
+	}
+
+    /**
+     * Set firstname.
+     */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+    /**
+     * Get lastname.
+     */
+	public String getLastname() {
+		return lastname;
+	}
+
+    /**
+     * Set lastname.
+     */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+    /**
+     * Get language.
+     */
+	public String getLanguage() {
+		return language;
+	}
+
+    /**
+     * Set language.
+     */
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+    /**
+     * Get gender.
+     */
+	public String getGender() {
+		return gender;
+	}
+
+    /**
+     * Set gender.
+     */
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder(1024);
         sb.append("Authentication [")
           .append("identity:").append(identity).append(", ")
           .append("email:").append(email)
+          .append("fullname:").append(fullname)
+          .append("language:").append(language)
+          .append("gender:").append(gender)
           .append(']');
         return sb.toString();
     }
